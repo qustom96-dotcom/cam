f7095d290e5890f9cffb84b34b4d4a6f8884745a
qustom96-dotcom <qustom96@hotmail.com>
2026-05-01 17:02:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,34 @@ class KameraProApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: CameraAwesomeBuilder.awesome(
-          // Postavke za video i sliku
-          saveConfig: SaveConfig.photoAndVideo(),
-          
-          // Kontrole koje si tražio (ISO, Shutter, Focus...)
-          onMediaTap: (media) {
-            print("Snimljeno: ${media.captureRequest.toString()}");
-          },
-          
-          // Senzor i rezolucija
-          sensorConfig: SensorConfig.single(
-            sensor: Sensor.position(SensorPosition.back),
-            aspectRatio: CameraAspectRatios.ratio_16_9,
-          ),
-          
-          // Ovo automatski dodaje UI sa svim kontrolama
-          previewFit: CameraPreviewFit.cover,
+      debugShowCheckedModeBanner: false,
+      home: CameraAwesomePage(),
+    );
+  }
+}
+
+class CameraAwesomePage extends StatelessWidget {
+  const CameraAwesomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: CameraAwesomeBuilder.awesome(
+        saveConfig: SaveConfig.photoAndVideo(),
+        sensorConfig: SensorConfig.single(
+          sensor: Sensor.position(SensorPosition.back),
+          aspectRatio: CameraAspectRatios.ratio_16_9,
+          zoom: 0.0,
         ),
+        previewFit: CameraPreviewFit.cover,
+        onMediaTap: (mediaCapture) {
+          mediaCapture.captureRequest.when(
+            single: (single) {
+              print("Snimljeno: ${single.file?.path}");
+            },
+          );
+        },
       ),
     );
   }
